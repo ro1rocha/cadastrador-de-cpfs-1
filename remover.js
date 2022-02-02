8fdad47aba7899bf66ee5a71a8cd315db1d075ff
Ro Rocha <ro.rochatur@gmail.com>
2022-02-02 10:16:51 -0300

    resolução remover pessoas

---
+++ remover.js
@@ -0,0 +1,10 @@
+// passo 1: Capturar o número digitado pelo usuário
+const numero = process.argv[2];
+// Passo 2: Importar array de pessoas
+const pessoas = require('./pessoas.json');
+// Passo 3: Remover a pessoa da posição desejada
+pessoas.splice(numero, 1);
+// Passo 4: Salvar o array modificado (sem a pessoa) no arquivo 
+const fs = require('fs');
+fs.writeFileSync('./pessoas.json', JSON.stringify(pessoas, null, 4));
+
